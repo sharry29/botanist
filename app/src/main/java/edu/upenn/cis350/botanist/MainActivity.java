6b85f990bbc2be65985b04b19957d54c30883021
Jacqueline Askins <askinsj@sas.upenn.edu>
2017-02-14 15:00:37 -0500

    initial commit

---
+++ app/src/main/java/edu/upenn/cis350/botanist/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.upenn.cis350.botanist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
