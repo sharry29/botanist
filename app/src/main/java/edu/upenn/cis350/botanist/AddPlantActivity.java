589257c84324b9723d9868e5b436315d137c585e
Katherine Dix <kathdix@hntvlan569.0411.wlan.wireless-resnet.upenn.edu>
2017-02-14 20:32:52 -0500

    Added view and layout for creating a new flower. Added button to create new flower(button doesn't do anything yet)

--- app/src/main/java/edu/upenn/cis350/botanist/AddPlantActivity.java
+++ app/src/main/java/edu/upenn/cis350/botanist/AddPlantActivity.java
@@ -2,18 +2,12 @@ package edu.upenn.cis350.botanist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class AddPlantActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("testing git");
-    }
-
-    public void addPlant(View view) {
-
+        setContentView(R.layout.activity_add_plant);
     }
 }
