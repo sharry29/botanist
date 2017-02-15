c2cb8dc0f162755fbbeb998cf5478a9df08999d0
Katherine Dix <kathdix@hntvlan569.0411.wlan.wireless-resnet.upenn.edu>
2017-02-14 20:50:10 -0500

    Added simple form to create a plant

--- app/src/main/java/edu/upenn/cis350/botanist/MainActivity.java
+++ app/src/main/java/edu/upenn/cis350/botanist/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.upenn.cis350.botanist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addPlant(View view) {
-
+        Intent intent = new Intent(this, AddPlantActivity.class);
+        startActivity(intent);
     }
 }
