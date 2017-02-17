72d8bdaf6140b9c5527e484e27e9910410af9fef
Katherine Dix <kathdix@hntvlan569.0837.wlan.wireless-resnet.upenn.edu>
2017-02-16 21:15:38 -0500

    added the ability to check wikipedia description of a plant

--- app/src/main/java/edu/upenn/cis350/botanist/AddPlantActivity.java
+++ app/src/main/java/edu/upenn/cis350/botanist/AddPlantActivity.java
@@ -1,7 +1,11 @@
 package edu.upenn.cis350.botanist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
@@ -21,4 +25,14 @@ public class AddPlantActivity extends AppCompatActivity {
     private static final String[] PLANTS = new String[] {
             "Sunflower", "Rose", "Carnation", "Tulip", "Daffodil"
     };
+
+    public void checkDescription(View view) {
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.plant_list);
+        String plant = String.valueOf(textView.getText());
+        if (plant.equals("Sunflower")) {
+            Uri uri = Uri.parse("https://en.wikipedia.org/wiki/Helianthus");
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        }
+    }
 }
