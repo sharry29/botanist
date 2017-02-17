704e7d8a9cabaaf0af4d0d593f6645c6612547e1
Katherine Dix <kathdix@hntvlan569.0837.wlan.wireless-resnet.upenn.edu>
2017-02-16 20:33:59 -0500

    added drop down menu as the user is typing

--- app/src/main/java/edu/upenn/cis350/botanist/AddPlantActivity.java
+++ app/src/main/java/edu/upenn/cis350/botanist/AddPlantActivity.java
@@ -2,6 +2,8 @@ package edu.upenn.cis350.botanist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class AddPlantActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class AddPlantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plant);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this,
+                android.R.layout.simple_dropdown_item_1line, PLANTS);
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.plant_list);
+        textView.setAdapter(adapter);
     }
+
+    private static final String[] PLANTS = new String[] {
+            "Sunflower", "Rose", "Carnation", "Tulip", "Daffodil"
+    };
 }
