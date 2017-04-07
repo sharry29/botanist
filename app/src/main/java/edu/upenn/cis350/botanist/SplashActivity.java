ff4b88d739eceac4de23fb51b5ecc8a0e6d2c726
harryhas29 <sharry@sas.upenn.edu>
2017-04-06 23:40:26 -0400

    added splash screen, putting icons in action bar

---
+++ app/src/main/java/edu/upenn/cis350/botanist/SplashActivity.java
@@ -0,0 +1,21 @@
+package edu.upenn.cis350.botanist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Harry on 4/6/17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
